3b32fcb1ae6e36044d68efce1b8309b42d44aa46
gkswlals104 <jiminhan71@gmail.com>
2026-07-04 04:09:41 +0900

    채팅 메세지뷰 ui 구현

---
+++ BookBetween/BookBetween/Features/Chat/Views/ChatMessageView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessageView.swift
+//  BookBetween
+//
+//  Created by 한지민 on 7/4/26.
+//
+
+import Foundation
---
+++ BookBetween/BookBetween/Features/Chat/Views/ChatView.swift
@@ -0,0 +1,8 @@
+//
+//  ChatView.swift
+//  BookBetween
+//
+//  Created by 한지민 on 7/4/26.
+//
+
+import Foundation
