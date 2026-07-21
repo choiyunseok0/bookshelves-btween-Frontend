4f5ea4d112b353344ffcde29ed50b87b6fc18974
choiyunseok0 <choiyunseok128@naver.com>
2026-07-21 17:09:17 +0900

    카카오 SDK 초기화 및 로그인 콜백 설정

--- BookBetween/BookBetween/BookBetweenApp.swift
+++ BookBetween/BookBetween/BookBetweenApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
+import KakaoSDKCommon
 
 @main
 struct BookBetweenApp: App {
+    init() {
+        guard let kakaoNativeAppKey = Bundle.main.object(
+            forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY"
+        ) as? String,
+        !kakaoNativeAppKey.isEmpty else {
+            preconditionFailure("KAKAO_NATIVE_APP_KEY가 설정되지 않았습니다.")
+        }
+
+        KakaoSDK.initSDK(appKey: kakaoNativeAppKey)
+    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
